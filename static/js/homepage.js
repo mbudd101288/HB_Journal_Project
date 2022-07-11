818d639b7a6b0b6b7259d00fb661a077b54fbf2b
Molli Budd <mbudd101288@gmail.com>
2022-07-11 18:22:33 -0500

    video settings updated

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let i = 0;
 let txt = '52 Weeks of Journal Intentions'; /* The text */
 const speed = 150; /* The speed/duration of the effect in milliseconds */
@@ -20,5 +22,31 @@ myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
 
+// THE MOST IMPORTANT LINE you should add when using a background video
+//  * HTML5 videos will "steal" the click event from other elements.
+//  * Preventing the default action will disable click events for the video.
 
-const myVid = document.getElementById('myVideo').play();
+document.querySelector('.background-video video').addEventListener(
+  'click',
+  e => e.preventDefault()
+);
+
+// Callback function to scale the video so that it covers the entire screen
+function scaleVideo() {
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
+  if (innerWidth > innerHeight) {
+    document.querySelector('.background-video video').setAttribute('width', innerWidth);
+    document.querySelector('.background-video video').setAttribute('height', '');
+  } else {
+    document.querySelector('.background-video video').setAttribute('height', innerHeight);
+    document.querySelector('.background-video video').setAttribute('width', '');
+  }
+}
+
+// // Caveat: this causes the page to look wonky on initial load.
+window.onload = scaleVideo;
+
+// // Resize the video if the window is resized
+window.onresize = scaleVideo;
+
