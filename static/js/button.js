06e07af7cbf4164e371653913bc46f09d4e1edce
Molli Budd <mbudd101288@gmail.com>
2022-07-12 15:20:53 -0500

    tab ability added to text area

--- static/js/button.js
+++ static/js/button.js
@@ -12,3 +12,16 @@ entryBtn.addEventListener('click', (evt) => {
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+// Allows users to tab in text area - consider not using this? makes it more difficult for user's that don't use a mouse
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode===9 || e.which===9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1; 
+        }
+    }
+}
